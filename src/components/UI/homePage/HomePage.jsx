07961e09c7dcb96a645ec478fffd795c520b8069
Lerts <oddff@mail.ru>
2023-04-03 22:02:36 +0300

    Работа с HomePage

--- src/components/UI/homePage/HomePage.jsx
+++ src/components/UI/homePage/HomePage.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import down_arrow from "../img/down-arrow 2.png";
 import installa from "../img/Installa.gif";
 import content_1 from "../img/content 1.png";
-import cl from "./HomePage.module.css";
+import seo from "../img/seo.png";
+import research from "../img/research.png";
+import "./HomePage.css";
 
 const HomePage = () => {
   return (
-    <div className={cl.content}>
-      <div className={cl.section_1}>
-        <div className={cl.section_1_info}>
+    <div className="content">
+      <div className="section_1">
+        <div className="section_1_info">
           <div>
             <p>DIGITAL MARKETING AGENCY</p>
             <h3>We Build Your Digital Campaign Performance</h3>
@@ -18,13 +20,13 @@ const HomePage = () => {
           </div>
           <img src={installa} />
         </div>
-        <img className={cl.arrow} src={down_arrow} alt="Стрелка вниз" />
+        <img className="arrow" src={down_arrow} alt="Стрелка вниз" />
       </div>
-      <div>
+      <div className="section_2 h3">
         <h3>Services</h3>
-        <div>
+        <div className="section_2_info">
           <div>
-            <img src={content_1} />
+            <img src={research} />
             <h4>User & Audience Incsights Analysis</h4>
             <p>
               Data-rich audience insight reports, segmenting of audience based
@@ -32,7 +34,7 @@ const HomePage = () => {
             </p>
           </div>
           <div>
-            <img src={content_1} />
+            <img src={seo} />
             <h4>Performance Marketing Campaigns</h4>
             <p>
               Real-time ROI measurements optimizing KPIs such as customer
